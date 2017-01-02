cef8282b5d9e79763aeb55cb4491055d6b6c352f
Utku TURKOGLU <uturkoglu@cinerpark.local>
2017-01-02 16:37:39 +0300

    stripe purchase once / subscription cases added

--- src/route/web/stripe.ts
+++ src/route/web/stripe.ts
@@ -25,21 +25,42 @@ export default class Route extends WebBase {
     }
 
     @Auth.Anonymous()
-    stripeSubscriptionRoute() {
+    stripeRoute() {
         var plan = this.req.query.plan;
+        var times = this.req.query.onetime;
+        console.log(times);
         if (typeof plan === 'undefined' || plan.length == 0) {
             this.res.sendStatus(401);
         } else {
             return lib.plans.retrieve(plan).then((results) => {
-                this.res.render('account/stripe', {
-                    title: 'MiBo Subscriptions',
-                    planName: results.name,
-                    planAmount: results.amount,
-                    planCurrency: results.currency,
-                    status: 'init',
-                    planId: results.id,
-                    publicKey: config.stripePublic
-                });
+                if (times == 1) {
+                    this.res.render('account/stripe', {
+                        title: 'MiBo Subscriptions',
+                        planName: results.name,
+                        planAmount: results.amount,
+                        planCurrency: results.currency,
+                        status: 'init',
+                        planId: results.id,
+                        publicKey: config.stripePublic,
+                        oneTime: true
+                    });
+                } else if (times === null || typeof times === 'undefined') {
+                    this.res.render('account/stripe', {
+                        title: 'MiBo Subscriptions',
+                        planName: results.name,
+                        planAmount: results.amount,
+                        planCurrency: results.currency,
+                        status: 'init',
+                        planId: results.id,
+                        publicKey: config.stripePublic,
+                        oneTime: false
+                    });
+                } else {
+                    this.res.render('account/stripe', {
+                        title: 'Error',
+                        status: this.errStatus.planErr
+                    });
+                }
             }).catch((err) => {
                 this.res.render('account/stripe', {
                     title: 'Error',
@@ -50,7 +71,7 @@ export default class Route extends WebBase {
     }
 
     @Auth.Anonymous()
-    purchaseRoute() {
+    subscribeRoute() {
         return lib.plans.retrieve(this.req.body.planid).then((result) => {
             var token = this.req.body.stripeToken;
             return lib.customers.create({
@@ -92,8 +113,52 @@ export default class Route extends WebBase {
         })
     }
 
+    @Auth.Anonymous()
+    purchaseRoute() {
+        return lib.plans.retrieve(this.req.body.planid).then((result) => {
+            return lib.charges.create({
+                amount: result.amount,
+                currency: result.currency,
+                source: this.req.body.stripeToken,
+                description: result.name
+            }, (err, charge) => {
+                if (err) {
+                    this.res.render('account/stripe', {
+                        title: 'Subscription Completed',
+                        status: this.errStatus.stripeErr,
+                        planName: result.name
+                    });
+                }
+                emailmanager.send('hello@wellbit.io', 'MiBo - New Purchase Notification', 'purchase.ejs', {
+                    title: 'New Plan Purchase',
+                    plan: charge.description,
+                    amount: (charge.amount / 100).toFixed(2),
+                    currency: charge.currency.toUpperCase(),
+                    email: charge.source.name
+                }).then(() => {
+                    emailmanager.send(charge.source.name, 'MiBo - Your Subscription', 'customerpurchase.ejs', {
+                        title: 'Your Subscription',
+                        amount: (charge.amount / 100).toFixed(2),
+                        currency: charge.currency.toUpperCase(),
+                        plan: charge.description
+                    }).then(() => {
+                        this.res.render('account/stripe', {
+                            title: 'Subscription Success',
+                            status: this.errStatus.success
+                        });
+                    }).catch((err) => {
+                        this.res.sendStatus(500);
+                    });
+                }).catch((err) => {
+                    this.res.sendStatus(500);
+                });
+            });
+        });
+    }
+
     static SetRoutes(router: express.Router) {
-        router.get("/purchase*", Route.BindRequest('stripeSubscriptionRoute'));
+        router.get("/purchase*", Route.BindRequest('stripeRoute'));
+        router.post("/subscribe", Route.BindRequest('subscribeRoute'));
         router.post("/purchase", Route.BindRequest('purchaseRoute'));
     }
 
